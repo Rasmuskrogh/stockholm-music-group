7135fceb57d2d6debef2ac521f47dbc4630ac9d9
[Rasmus Krogh-Andersen] <rasmus.kroghandersen@gmail.com>
2026-01-19 15:59:30 +0100

    added phone and email buttons above the footer

--- src/components/sections/Footer/Footer.tsx
+++ src/components/sections/Footer/Footer.tsx
@@ -1,12 +1,16 @@
 import Section from "@/components/ui/Section/Section";
 import Link from "next/link";
+import Image from "next/image";
 
 import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <Section>
-      {/* <Container> */}
+      <div className={styles.footerContact}>
+        <span><Image src="/images/phone.svg" alt="Phone icon" width={20} height={20} /> <Link href="tel: +46701234567">Telefon</Link></span>
+        <span><Image src="/images/mail.svg" alt="Mail icon" width={20} height={20} /> <Link href="mailto: stockholm@music.group">E-post</Link></span>
+      </div>
       <div className={styles.footerContent}>
         <div className={styles.spacer}></div>
         <div className={styles.copyright}>
@@ -21,7 +25,7 @@ function Footer() {
           </p>
         </div>
       </div>
-      {/* </Container> */}
+      
     </Section>
   );
 }
